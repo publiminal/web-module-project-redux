45211513b6ab624fdd581684eaa1a8467d9883d8
publiminal <publiminal@gmail.com>
2022-04-21 06:43:55 -0400

    in progress, added connect, title, and remove features.

--- src/reducers/movieReducer.js
+++ src/reducers/movieReducer.js
@@ -6,18 +6,19 @@ const initialState = {
     appTitle: "IMDB Movie Database"
 }
 
-const movieReducer = (state=initialState, action) => {
+const reducer = (state=initialState, action) => {
     console.log('action', action )
     console.log('action type', action.type )
     console.log('action payload', action.payload )
     switch(action.type) {
         case DELETE_MOVIE:
             return {
-                movies: state.movies.filter(item=>(action.payload !== item.id))
-            }
+                movies: state.movies.filter(item=> (action.payload !== item.id.toString()) ),
+                appTitle:state.appTitle
+            };
         default:
             return state;
     }
 }
 
-export default movieReducer;
+export default reducer;
